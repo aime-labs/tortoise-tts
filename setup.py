5a5f92780ba3decf25126b7f10c1ea59132589f4
aime <toine@aime.info>
2024-07-31 20:35:13 +0000

    updated deepspeed and transformers to be compatible with Pytorch 2.1.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         'unidecode',
         'scipy',
         'librosa',
-        'transformers==4.31.0',
+        'transformers==4.33.0',
         'tokenizers',
         # 'deepspeed==0.8.3',
     ],
